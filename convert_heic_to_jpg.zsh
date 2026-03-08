c71e33e762fff28d5259114a41cd3c9629907bd2
Danijel-James Wynyard <danijel@repasscloud.com>
2026-03-08 19:11:51 +1100

    update HEIC to JPG conversion script to target specific directory

--- convert_heic_to_jpg.zsh
+++ convert_heic_to_jpg.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-for f in *.HEIC; do magick "$f" -auto-orient -colorspace sRGB -quality 92 -strip "${f%.HEIC}.jpg"; done
-
+for f in ./INST/img/*.HEIC; do magick "$f" -auto-orient -colorspace sRGB -quality 92 -strip "${f%.HEIC}.jpg"; done
+rm ./INST/img/*.HEIC
